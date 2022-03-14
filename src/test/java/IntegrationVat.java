5f31d0c6281f8aac8d2c43f2cb209b49e84be03d
sairam <sairamch10@gmaill.com>
2022-03-14 21:30:36 +0100

    test case failed

--- src/test/java/IntegrationVat.java
+++ src/test/java/IntegrationVat.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class IntegrationVat {
     public static  VatCalculator cc;
     @Test
+
     public void addtestPassInt()
     {
         Assert.assertEquals(120, cc.VAT_UK(100));
@@ -11,9 +12,9 @@ public class IntegrationVat {
         Assert.assertEquals(118, cc.VAT_IND(100));
         Assert.assertEquals(125, cc.VAT_SWE(100));
         Assert.assertEquals(120, cc.VAT_RUS(100));
-        Assert.assertEquals(120, cc.VAT_UK(100));
+        Assert.assertEquals(1, cc.VAT_UK(100));
 
 
     }
 
-}
+}//test case failed
